c3c4b714bd80239720c36a11ec82da2f94a8669f
HandyRandyx <realmhernan@hotmail.com>
2024-06-27 09:44:00 -0300

    CDN refresh

---
+++ utils/fetchInterceptor2.js
@@ -0,0 +1,34 @@
+(() => {
+  if (window.stashListener) return;
+
+  const { fetch: originalFetch } = window;
+  const stashListener = new EventTarget();
+
+  window.fetch = async (...args) => {
+    let [resource, config] = args;
+    const response = await originalFetch(resource, config);
+    const contentType = response.headers.get('content-type');
+
+    if (typeof resource === 'string'
+      && contentType
+      && contentType.indexOf('application/json') !== -1
+      && resource.endsWith('/graphql')) {
+      try {
+        const data = await response.clone().json();
+        console.log('Dispatching response event:', data);
+
+        if (data.data.configuration) {
+          stashListener.dispatchEvent(new CustomEvent('configuration', { detail: data.data.configuration }));
+        } else {
+          stashListener.dispatchEvent(new CustomEvent('response', { detail: data }));
+        }
+      }
+      catch (e) {
+        console.error('Error parsing JSON:', e);
+      }
+    }
+    return response;
+  };
+  
+  window.stashListener = stashListener;
+})();
